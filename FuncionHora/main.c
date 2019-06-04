53a9571200cf78780844f53ba607bfe6ff29010a
psang <ps.sanguino@gmail.com>
2019-06-04 13:52:35 -0300

    empezamos a trabajar en mi rama

---
+++ FuncionHora/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("Avion despegando ..↑↑↑..\n");
+    sleep(1);
+    printf(".\n");
+    sleep(1);
+    printf(".\n");
+    sleep(1);
+    printf(".\n");
+    sleep(1);
+    printf(".\n");
+    sleep(1);
+    printf("Avion en vuelo ✈ \n");
+    return 0;
+}

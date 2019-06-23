bc2efc0af293b5e6613dabaf21e0295e29346735
monticp <49138717+monticp@users.noreply.github.com>
2019-06-23 18:52:22 -0300

    Arreglo de error en bùsquedas

--- TPOS_TORRE/consola.c
+++ TPOS_TORRE/consola.c
@@ -1,78 +1,59 @@
-#include "consola.h"
-#include "avion.h"
-#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#define tamanioBufferMensaje 100
+#include <stdint.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <string.h>
+#include "consola.h"
 
 /******************************************//**
-*@fn conectar
-**se realiza coneccion del avión con el aeropuerto
-*@param int cliente
-*@param struct sockaddr_in direccionServidor
-*@return 1
+*@fn  abrirConsola
+**Visualiza una consola donde se puede acceder a
+** las listas de aviones y al usuario de la pista
+*@param ST_TODASLASLISTAS * todasLasListas
 ********************************************/
-int conectar (int cliente, struct sockaddr_in direccionServidor){
-        if (connect(cliente, (void*) &direccionServidor, sizeof(direccionServidor)) != 0) {
-                perror("No se pudo conectar");
-                return 1;
+void abrirConsola (ST_TODASLASLISTAS * todasLaslistas){
+        int flag = 0;
+        while (flag == 0){
+            char opcion = "0";
+                printf("\nBievenido al sistema de gestion de la torre de control.\n");
+                printf("[1] - Mostrar lista de aviones registrados\n");
+                printf("[2] - Mostrar lista de aterrizaje\n");
+                printf("[3] - Mostrar lista de despegue\n");
+                printf("[4] - Mostrar uso de pista\n");
+                printf("[5] - Eliminar avion\n");
+            opcion = getchar();
+            switch (opcion){
+                case '1':
+                    system ("clear");
+                    mostrarLista(&todasLaslistas->avionesRegistrados);
+                    getchar();
+                break;
+                case '2':
+                    system ("clear");
+                    mostrarLista(&todasLaslistas->listaAterrizaje);
+                    getchar();
+                break;
+                case '3':
+                    system ("clear");
+                    mostrarLista(todasLaslistas->colaDespegue);
+                    getchar();
+                break;
+                case '4':
+                    system ("clear");
+                    if (todasLaslistas->pistaAsignada!= 0){
+                        printf("\nLa pista està siendo utilizada por el aviòn ID: %i\n", todasLaslistas->pistaAsignada);
+                    }
+                    getchar();
+                break;
+                case '5':
+                    system ("clear");
+                    eliminarAvion(todasLaslistas);
+                    getchar();
+                break;
+                default:
+                 while (!getchar());
+                break;
             }
+        }
 }
-
-void mostrarOpcionesConsola(){
-    printf("\n[1] - Registrar avion");
-    printf("\n[2] - Solicitar pista");
-    printf("\n[3] - Despegar");
-    printf("\n[4] - Aterrizar");
-    printf("\n[5] - Mostrar estado");
-    printf("\n[6] - Salir\n");
-}
-
-
-/*******************************************//**
-*@fn abrirConsola
-**abre un menú de opciones, muestra las característica del avión actual
-*@param struct sockaddr_in direccionServidor
-*@param int cliente
-*@param char * IP
-*@param char*puerto
-*@param ST_AVION *avion
-*@param int * pistaAsignada
-********************************************/
-void abrirConsola(struct sockaddr_in direccionServidor, int cliente, char * IP, char*puerto, ST_AVION *avion){
-    printf("\n Bienvenido al sistema de control del avion\n");
-    mostrarEstadoAvion(avion);
-    mostrarOpcionesConsola();
-    int N=0;
-    scanf("%d",&N);
-    switch (N){
-        case 1:
-        system("clear");
-        conectar(cliente, direccionServidor);
-        registracion(cliente, IP, puerto, avion);
-        abrirConsola(direccionServidor, cliente, IP, puerto, avion);
-        break;
-        case 2:
-        system("clear");
-        solicitarPista(cliente, IP, puerto, avion);
-        abrirConsola(direccionServidor, cliente, IP, puerto, avion);
-        break;
-        case 3:
-        system("clear");
-        despegar(cliente, IP, puerto, avion);
-        abrirConsola(direccionServidor, cliente, IP, puerto, avion);
-        break;
-        case 4:
-        system("clear");
-        aterrizar(cliente, IP, puerto, avion);
-        abrirConsola(direccionServidor, cliente, IP, puerto, avion);
-        break;
-        case 5:
-        system("clear");
-        mostrarEstadoAvion(avion);
-        abrirConsola(direccionServidor, cliente, IP, puerto, avion);
-        break;
-    }
-}
-
-
--- TPOS_TORRE/consola.h
+++ TPOS_TORRE/consola.h
@@ -1,29 +1,8 @@
 #ifndef CONSOLA_H_INCLUDED
 #define CONSOLA_H_INCLUDED
-#include "avion.h"
 
-#include <arpa/inet.h>
-#include <sys/socket.h>
+#include "gestionPista.h"
 
-
-/******************************************//**
-** TAD: consola
-**Este TAD nos permite abrir un menú de opciones, muestra las característica del avión actual
-
-
-**CONJUNTOS DE DATOS:
-**----------Estructuras-----------:
-struct sockaddr_in direccionServidor, ST_AVION
-**----------Variables-------------:
- cliente
-**----------Punteros--------------:
-* IP, *puerto, *avion, * pistaAsignada.
-
-**OPERACIONES:
-<void abrirConsola>  <(struct sockaddr_in direccionServidor, int cliente, char * IP, char*puerto, ST_AVION *avion, int * pistaAsignada)>
-<int conectar>  <(int cliente, struct sockaddr_in direccionServidor)>
-*******************************************/
-
-void abrirConsola(struct sockaddr_in direccionServidor, int cliente, char * IP, char*puerto, ST_AVION *avion);
+void abrirConsola (ST_TODASLASLISTAS * todasLaslistas);
 
 #endif // CONSOLA_H_INCLUDED
